bcb8b649a6e82ac47d711377888b66e346a771f5
Andrea Ayala <alejandrayala01@gmail.com>
2020-04-17 12:57:47 -0600

    Metodos de clase: Servicio Profesional

--- EmpresaPOO/src/com/AAAD/x00109319/ServicioProfesional.java
+++ EmpresaPOO/src/com/AAAD/x00109319/ServicioProfesional.java
@@ -1,4 +1,18 @@
 package com.AAAD.x00109319;
 
-public class ServicioProfesional {
+public class ServicioProfesional extends Empleado {
+    private int meses_Contrato;
+
+    public ServicioProfesional(String nombre, String puesto, int salario, int meses_Contrato) {
+        super(nombre, puesto, salario);
+        this.meses_Contrato=meses_Contrato;
+    }
+
+    public int getMeses_Contrato() {
+        return meses_Contrato;
+    }
+
+    public void setMeses_Contrato(int mesesdeC) {
+        meses_Contrato= mesesdeC;
+    }
 }
